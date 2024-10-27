0ed3ae45f874dc8ce0a7beea7170a473f83f2637
Nathaniel Risum <nathaniel.risum2@gmail.com>
2024-10-27 23:24:43 +0100

    Solve extra solution

---
+++ index.js
@@ -0,0 +1,44 @@
+Vue.createApp({
+
+    data() {
+
+        return {
+
+            // add here
+            Word: 'kOKAIN', 
+            Words: [], 
+            Message: 'Button clicked!',
+            ShouldShow: false,
+        }
+
+    },
+    
+    methods: {
+
+        // add here
+        Save(){
+            this.Words.push(this.Word);
+            console.log(this.Word);
+            
+        },
+        Show(){
+            this.ShouldShow = true;
+            console.log(this.Words.toString())
+        },
+        Clear(){
+            this.Words = [];
+            this.ShouldShow = false; 
+            console.log(this.WordsToShow)
+        }
+    },
+    computed: {
+        WordsToShow(){
+            if (this.Words.toString() != ''){
+                return this.Words.toString();
+            }
+            return 'Empty'
+        }
+    },
+    
+
+}).mount("#app")
